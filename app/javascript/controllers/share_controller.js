109bcb83c96dee9367a0357e73c639bf683931d5
Anthony Marion <anthonycmarion@gmail.com>
2023-04-13 19:53:39 +1000

    Stimulus and blog post view updated

--- app/javascript/controllers/share_controller.js
+++ app/javascript/controllers/share_controller.js
@@ -2,7 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="share"
 export default class extends Controller {
+  static targets = ["url", "title,", "content"];
+
   connect() {
-    console.log("Connected to the share controller")
+    console.log("Connected to the share controller");
+    console.log(this.data.get("urlValue"));
   }
-}
+  async share(e) {
+    e.preventDefault();
+
+    const shareData = {
+      title: this.titleTarget.value,
+      body: this.contentTarget.textContent,
+      url: this.data.get("urlValue"),
+    };
+    console.log(shareData);
+  }
+ }
