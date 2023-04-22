163860597613b06c5c395f7503c8ffd753a4f2d0
Anthony Marion <anthonycmarion@gmail.com>
2023-04-22 11:54:04 +1000

    Reading time function completed and styled using Tailwind

--- app/javascript/controllers/reader_controller.js
+++ app/javascript/controllers/reader_controller.js
@@ -2,7 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="reader"
 export default class extends Controller {
+  static targets = [ "readingTime", "content" ]
+
   connect() {
     console.log("Connected to the reader controller");
+    this.readingTime();
+  }
+
+  readingTime() {
+    const text = this.contentTarget.textContent;
+    const wordsPerMinute = 235;
+    const numberOfWords = text.split(/\s/g).length;
+    const minutes = numberOfWords / wordsPerMinute;
+    const readTime = Math.ceil(minutes);
+    this.readingTimeTarget.textContent = `${readTime} min read`;
   }
 }
