f4c1f4f54cad316c4a217ff43bdb0cb00e322ff2
Anthony Marion <anthonycmarion@gmail.com>
2023-05-01 19:00:21 +1000

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :blog_posts
+  resources :blog_posts, only: [:index, :show, :new, :create, :destroy]
 
   # Defines the root path route ("/")
   root 'blog_posts#index'
+  get 'new', to: 'blog_posts#new', as: :new_blog_post
 end
