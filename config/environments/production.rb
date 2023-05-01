016a2510c7fbe42889744267a1a4a7aaeda67773
Anthony Marion <anthonycmarion@gmail.com>
2023-05-01 21:38:37 +1000

    production.rb update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,6 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :cloudinary
-  config.cloudinary_url = ENV['CLOUDINARY_URL']
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
